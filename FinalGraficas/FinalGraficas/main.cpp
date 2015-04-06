626be74481ec30c6f1d491afa046061b55fefcad
Marialicia Villarreal <marialicia_93@hotmail.com>
2015-04-05 19:10:54 -0500

    cubo del juego

--- FinalGraficas/FinalGraficas/main.cpp
+++ FinalGraficas/FinalGraficas/main.cpp
@@ -20,6 +20,8 @@ using namespace std;
 Pelota *aux = new Pelota(0,0,0,0,0,0,0);
 int listaPelota, listaFondo;
 
+const float medida = 1.0;
+
 void init(void)
 {
     glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -33,18 +35,20 @@ void reshape (int w, int h)
 
     glMatrixMode (GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-800, 800, -800, 800, 0, 400);
+    gluPerspective(60.0, 1.0, 1.0, 10.0);
 
     glMatrixMode (GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0, 0, 200, 0, 0, 0, 0, 1, 0);
+    gluLookAt(0, 0, 2.7, 0, 0, 0, 0, 1, 0);
 }
 
 void myTimer(int v)
 {
-
+    aux->setPosX(aux->getPosX()+aux->getVelX());
+    aux->setPosY(aux->getPosY()+aux->getVelY());
+    aux->setPosZ(aux->getPosZ()+aux->getVelZ());
     glutPostRedisplay();
-    glutTimerFunc(500, myTimer, 1);
+    glutTimerFunc(5, myTimer, 1);
 }
 
 void createList()
@@ -70,11 +74,48 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glColor3f(0.0, 1.0, 0.0);
-
     glPushMatrix();
-    glTranslatef(0, 0, 0);
-    glutWireCube(50);
+    glRotatef(1.0, 1.0, 1.0, 0.0);
+
+    glBegin(GL_QUADS);
+
+    //Arriba (amarillo)
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex3f( -medida, medida, -medida );
+    glVertex3f(  medida, medida, -medida );
+    glVertex3f(  medida, medida,  medida );
+    glVertex3f( -medida, medida,  medida );
+
+    glColor3f(1.0, 1.0, 1.0);
+    //Atrás (blanco)
+    glVertex3f( -medida, -medida, -medida );
+    glVertex3f(  medida, -medida, -medida );
+    glVertex3f(  medida,  medida, -medida );
+    glVertex3f( -medida,  medida, -medida );
+
+    //Derecha (verde)
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f( medida, -medida, -medida );
+    glVertex3f( medida,  medida, -medida );
+    glVertex3f( medida,  medida,  medida );
+    glVertex3f( medida, -medida,  medida );
+
+    //Izquierda (azul)
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f( -medida, -medida, -medida );
+    glVertex3f( -medida, -medida,  medida );
+    glVertex3f( -medida,  medida,  medida );
+    glVertex3f( -medida,  medida, -medida );
+
+    //Abajo (magenta)
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f( -medida, -medida, -medida );
+    glVertex3f(  medida, -medida, -medida );
+    glVertex3f(  medida, -medida,  medida );
+    glVertex3f( -medida, -medida,  medida );
+
+    glEnd();
+
     glPopMatrix();
 
     glutSwapBuffers();
@@ -98,11 +139,11 @@ int main(int argc, char** argv)
     glutInitWindowSize (800,800);
     glutInitWindowPosition (0,0);
     glutInitDisplayMode (GLUT_DOUBLE| GLUT_RGB| GLUT_DEPTH);
-    glutCreateWindow (argv[0]);
+    glutCreateWindow("Smash Junk Food");
     init();
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
-    //glutTimerFunc(5, myTimer, 1);
+    glutTimerFunc(500, myTimer, 1);
     glutKeyboardFunc(keyboard);
     glutMainLoop();
     return 0;
